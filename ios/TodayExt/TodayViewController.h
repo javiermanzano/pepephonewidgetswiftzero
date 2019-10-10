467db3974e37a884bea3c7c781fc30683f907db1
Javier Manzano <javier.manzano22@gmail.com>
2019-10-10 16:05:44 +0200

    chore(): fixed widget

---
+++ ios/TodayExt/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  TodayExt
+//
+//  Created by Javier on 10/10/2019.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
