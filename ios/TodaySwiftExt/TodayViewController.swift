f95052190466c23295880d8b65c02cffa2e1fd27
Javier Manzano <javier.manzano22@gmail.com>
2019-10-11 10:55:52 +0200

    chore(): first SVG loaded from file

--- ios/TodaySwiftExt/TodayViewController.swift
+++ ios/TodaySwiftExt/TodayViewController.swift
@@ -9,39 +9,42 @@
 import UIKit
 import NotificationCenter
 
-/*class myView: UIView {
-  func prepare() {
-      backgroundColor = UIColor.blueColor()
-  }
-
-  override func drawRect(rect: CGRect) {
-  // custom stuff
-  }
-}*/
-
 class MyView: MacawView {
 
-  required init?(coder aDecoder: NSCoder) {
-    let text = Text(text: "Hello, World!", place: .move(dx: 0, dy: 0))
-    super.init(node: text, coder: aDecoder)
+  open func play() {
+    /*let text = Text(text: "Hello, World!", place: .move(dx: 0, dy: 0))
+    super.init(node: text, coder: aDecoder)*/
+    do {
+      self.node = try SVGParser.parse(path: "camera")
+    } catch {
+      let text = Text(text: "Hello, World!", place: .move(dx: 0, dy: 0))
+      self.node = text
+    }
   }
 
 }
 
 class TodayViewController: UIViewController, NCWidgetProviding {
   
- override func loadView()
-  {
-    let viewPlaying = TotalOfStepsView()
-      view = viewPlaying
-    viewPlaying.play()
-    self.extensionContext?.widgetLargestAvailableDisplayMode = .expanded
-  }
+    override func loadView() {
+      let customView = MyView()
+      view = customView
+      customView.play()
+      view.backgroundColor = UIColor.white
+      self.extensionContext?.widgetLargestAvailableDisplayMode = .expanded
+    }
         
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+  
+  func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
+    if activeDisplayMode == .expanded {
+        preferredContentSize = CGSize(width: 0, height: 380)
+    } else {
+        preferredContentSize = maxSize
+    }
+  }
         
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
         // Perform any setup necessary in order to update the view.
