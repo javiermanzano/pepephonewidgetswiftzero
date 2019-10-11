84d75ee0615433212ba2d95a64192708eac2f6d0
Javier Manzano <javier.manzano22@gmail.com>
2019-10-11 11:36:33 +0200

    chore(): write SVG to file

--- ios/TodaySwiftExt/TodayViewController.swift
+++ ios/TodaySwiftExt/TodayViewController.swift
@@ -24,6 +24,29 @@ class MyView: MacawView {
 
 }
 
+let file = "readit"
+let text = "<g id=\"usage/progress/icon/data\" transform=\"translate(17.000000, 18.000000)\">" +
+"   <g id=\"ic_data-consume\">" +
+"      <path d=\"M16,17.01 L16,10 L14,10 L14,17.01 L11,17.01 L15,21 L19,17.01 L16,17.01 L16,17.01 Z M9,3 L5,6.99 L8,6.99 L8,14 L10,14 L10,6.99 L13,6.99 L9,3 Z\" id=\"Shape\" fill=\"#737373\" fill-rule=\"nonzero\"></path>" +
+"      <polygon id=\"Shape\" points=\"0 0 24 0 24 24 0 24\"></polygon>" +
+"   </g>" +
+"</g>";
+
+func writeToFile() {
+  if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+      let fileURL = dir.appendingPathComponent(file)
+      do {
+          try text.write(to: fileURL, atomically: false, encoding: .utf8)
+      }
+      catch {/* error handling here */}
+      do {
+          let text2 = try String(contentsOf: fileURL, encoding: .utf8)
+          NSLog("-->> %@", text2)
+      }
+      catch {/* error handling here */}
+  }
+}
+
 class TodayViewController: UIViewController, NCWidgetProviding {
   
     override func loadView() {
@@ -32,6 +55,7 @@ class TodayViewController: UIViewController, NCWidgetProviding {
       customView.play()
       view.backgroundColor = UIColor.white
       self.extensionContext?.widgetLargestAvailableDisplayMode = .expanded
+      writeToFile()
     }
         
     override func viewDidLoad() {
